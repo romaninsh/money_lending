2bf758cab691852f64e6acc52b7b47e2b3e2f442
Romans Malinovskis <romans@agiletoolkit.org>
2017-04-27 10:45:13 +0100

    add cleardb detection for db

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
 <?php
 require'vendor/autoload.php';
 
-phpinfo();
+if (isset($_ENV['CLEARDB_DATABASE_URL'])) {
+    // we are on Heroku
+    var_dump($_ENV['CLEARDB_DATABASE_URL']);
+
+}else{
+    // we are on local
+}
 
 //$app = new \atk4\ui\App('testing');
 //$app->initLayout('Centered');
